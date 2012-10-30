b0ae4671ed92614c146059cab4695e0e9d16c715
Aref Aslani <arefaslani@gmail.com>
2012-10-30 10:36:07 +0330

    Project initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DinoProff::Application.config.secret_token = '9d825e8d54909e6a50136a6b1191ff5e6fcdca398fb4e84a205acbf6121956cfe500e43f34f9c4af659b3bef4949ffa03e47a407a09793032732e60c4cd0aedc'
